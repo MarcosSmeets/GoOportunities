11ae5f39337333b5cc14bf6d64d6de0abce474f4
marco <marcos.smeets@nycdigital.com.br>
2024-04-26 18:07:31 -0300

    feat: add log and config

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/MarcosSmeets/GoOportunities.git/router"
+import (
+	"fmt"
+	"github.com/MarcosSmeets/GoOportunities.git/config"
+	"github.com/MarcosSmeets/GoOportunities.git/router"
+)
 
 func main() {
+	//Initialize config
+	err := config.InitDb()
+
+	if err == nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
